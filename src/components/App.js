42430384cf2359c3561773bf295f7ed366cbdf0b
Ruth <ruro122020@gmail.com>
2023-10-27 06:06:10 -0400

    fetch toys from server

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,12 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3001/toys')
+    .then(res => res.json())
+    .then(data => setToys(data))
+  },[])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
